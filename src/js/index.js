d4d849285cf7b4e69039e50932276060b4e0ac94
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2022-07-26 12:24:57 +0300

    modal placeholder

--- src/js/index.js
+++ src/js/index.js
@@ -16,6 +16,7 @@ function testWebP(callback) {
     }
 });
 
+// Slider
 let slider = tns({
     container: '.home-slider__slides',
     items: 1,
@@ -41,8 +42,6 @@ let slider_benefits = tns({
 })
 
 let slider_services = tns({
-    // services-slider
-    // services-title__control-arrow
     container: '.services-slider',
     items: 4,
     slideBy: 1,
@@ -54,8 +53,6 @@ let slider_services = tns({
 })
 
 let slider_technologies = tns({
-    // services-slider
-    // services-title__control-arrow
     container: '.technologies-slider',
     items: 4,
     slideBy: 1,
@@ -76,3 +73,21 @@ let slider_reviews = tns ({
     speed: 400,
     controlsContainer: '.reviews-title__control-arrow',
 })
+
+// Modal
+document.addEventListener('click', function(e){
+    if(e.target.closest('.modal_activate')){
+        document.querySelector('.modal').classList.add('active')
+        document.querySelector('body').classList.add('modal-active')
+    } else if (!e.target.closest('.modal-content') ) {
+        document.querySelector('.modal').classList.remove('active')
+        document.querySelector('body').classList.remove('modal-active')
+    }
+})
+
+close_modal.addEventListener('click', () => {
+    document.querySelector('.modal').classList.remove('active')
+    document.querySelector('body').classList.remove('modal-active')
+})
+
+
